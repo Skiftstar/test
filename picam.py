bd5296a70bc7772b4d2134fcf442b30d9396a48f
Mark Seidel <mark.seidel@evia.de>
2024-05-07 11:51:25 +0200

    .

--- picam.py
+++ picam.py
@@ -14,10 +14,7 @@ while True:
     im = picam2.capture_array()
 
     grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    faces = (0, 0, 100, 100)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0))
+    cv2.rectangle(im, (0, 0), (100, 100), (0, 255, 0))
 
     cv2.imshow("Camera", im)
     cv2.waitKey(1)
