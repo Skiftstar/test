b06f36cd2ee63242677b4b270d5bd568f368f476
Mark Seidel <mark.seidel@evia.de>
2024-05-07 11:57:04 +0200

    changes

--- picam.py
+++ picam.py
@@ -6,8 +6,9 @@ from picamera2 import Picamera2
 
 cv2.startWindowThread()
 
-picam2 = Picamera2()
+picam2 = Picamera2(framerate=30)
 picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.video_configuration.controls.FrameRate = 120.0
 picam2.start()
 
 while True:
