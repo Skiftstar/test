573f4ce811304f7f1d44cc043de5b895ec8e1e3f
Skiftstar <yukamitft@gmail.com>
2024-05-14 13:07:04 +0200

    old camera code

--- RH/Processing/ProcessFrame.py
+++ RH/Processing/ProcessFrame.py
@@ -51,8 +51,8 @@ def processFrame(frame, sliders):
         filteredFrame=frame, 
         boundaries=[(lowerBoundary, upperBoundary, puckMinRadius, puckMaxRadius), (robotLowerBoundary, robotUpperBoundary, robotMinRadius, robotMaxRadius)], 
         resizeFrame=resizeFrame,
-        useBlur=False,
-        useUMat=False,
+        useBlur=True,
+        useUMat=True,
         detectRobot=lastRobotDetection == 0
     )
 
